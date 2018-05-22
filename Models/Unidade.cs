18d6b36365ff5a45e52d1c70fdf69ff188d3a060
Leonardo Miranda do Nascimento <leonardo@moki.com.br>
2018-05-22 15:22:13 -0300

    Commit project

---
+++ Models/Unidade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AutoCompleteTextViewSample.Models
+{
+    public class Unidade
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+    }
+}
